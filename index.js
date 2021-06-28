d58eb25e04c91d743f7826d3ddce6a89e07d4919
majo1569 <ojedamariaj1569@gmail.com>
2021-06-28 20:42:21 -0300

    express

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
 var calculator = require('./app/calculator');
 
 var a = 3;
